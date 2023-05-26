132c6f087c236231c7fcf6995fb0878f332aca63
Kened Silva Felix <ksfelix1@stefanini.com>
2023-05-26 12:26:50 -0300

    Adicionado fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from "styled-components";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
 
 import  themes  from './src/global/styles/theme';
 
 import { Dashboard } from "./src/pages/Dashboard/index";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading></AppLoading>
+  }
   return (
     <ThemeProvider theme={themes}>
       <Dashboard />
