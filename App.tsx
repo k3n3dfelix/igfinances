c59873cabaffa1069169040c93706eafa4242e3b
Kened Silva Felix <ksfelix1@stefanini.com>
2023-05-26 11:54:35 -0300

    feito tema padrão para a aplicação

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Dashboard } from './src/pages/Dashboard/index';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import  themes  from './src/global/styles/theme';
+
+import { Dashboard } from "./src/pages/Dashboard/index";
 
 export default function App() {
   return (
-    <Dashboard />
+    <ThemeProvider theme={themes}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
-
-
