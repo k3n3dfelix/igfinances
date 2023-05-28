77cbe009f6e4ffdfd7d2171ab509795aa2a31a17
Kened Silva Felix <ksfelix1@stefanini.com>
2023-05-27 21:03:42 -0300

    iniciado Header da aplicação

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,34 @@
-import {  Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
-import { Container, Title }  from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/6686227?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Kened</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
-
