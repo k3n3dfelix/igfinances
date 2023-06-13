f223e2a8a11f9d4a8ad7da6194676dac81322d85
Kened Silva Felix <ksfelix1@stefanini.com>
2023-06-13 15:53:36 -0300

    Finalizado o Cadastro de transações na async storage

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
-import { 
-  Keyboard, 
-  Modal, 
-  TouchableWithoutFeedback,
-  Alert
- } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Keyboard, Modal, TouchableWithoutFeedback, Alert } from "react-native";
 
- import * as Yup from 'yup';
- import { yupResolver } from '@hookform/resolvers/yup';
-
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import uuid from "react-native-uuid";
 import { useForm } from "react-hook-form";
+import { useNavigation } from '@react-navigation/native';
 
 import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
 import { CategorySelectButton } from "../../components/Forms/CategorySelectButton";
@@ -28,29 +25,39 @@ import {
 } from "./styles";
 
 interface FormData {
+  id: string;
   name: string;
   amount: string;
 }
+type NavigationProps = {
+  navigate:(screen:string) => void;
+}
 
 const schema = Yup.object().shape({
-  name: Yup.string().required('Nome é obrigatório'),
-  amount: Yup
-  .number()
-  .typeError('Informe m valor numérico')
-  .positive('O valor não pode ser negativo')
-  .required('O valor é obrigatório')
-})
+  name: Yup.string().required("Nome é obrigatório"),
+  amount: Yup.number()
+    .typeError("Informe m valor numérico")
+    .positive("O valor não pode ser negativo")
+    .required("O valor é obrigatório"),
+});
 
 export function Register() {
   const [transactionType, setTransactionType] = useState("");
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+  const dataKey = "@igfinances:transactions";
+  const navigation = useNavigation<NavigationProps>();
   const [category, setCategory] = useState({
     key: "category",
     name: "Categoria",
   });
 
-  const { control, handleSubmit, formState: { errors} } = useForm({
-    resolver: yupResolver(schema)
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
   });
 
   function handleOpenSelectCategoryModal() {
@@ -64,27 +71,57 @@ export function Register() {
     setTransactionType(type);
   }
 
-  function handleRegister(form: FormData) {
+  async function handleRegister(form: FormData) {
+    if (!transactionType) return Alert.alert("Selecione o tipo da transação");
 
-    if(!transactionType)
-      return Alert.alert('Selecione o tipo da transação');
-    
-    if(category.key === 'category')
-      return Alert.alert('Selecione a categoria');
+    if (category.key === "category")
+      return Alert.alert("Selecione a categoria");
 
-    const data = {
+    const newTransaction: FormData = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
       category: category.key,
+      date: new Date(),
     };
-    console.log(data);
+
+    try {
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [...currentData, newTransaction];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+      reset();
+      setTransactionType("");
+      setCategory({
+        key: "category",
+        name: "Categoria",
+      });
+      navigation.navigate('Listagem');
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possivel salvar");
+    }
   }
+
+  useEffect(() => {
+    async function loadData() {
+      const data = await AsyncStorage.getItem(dataKey);
+      console.log(JSON.parse(data!));
+    }
+    loadData();
+    // async function removeAll(){AsyncStorage.removeItem(dataKey);}
+    // removeAll();
+  });
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <Container>
         <Header>
-          <Title>Cadastro</Title>
+          <Title>Cadastros</Title>
         </Header>
         <Form>
           <Fields>
